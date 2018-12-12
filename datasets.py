97f871772d0879399789b4f4d85022fd0ffaed1d
cedesu <34418825+cedesu@users.noreply.github.com>
2018-12-12 00:26:57 -0500

    Add files via upload

---
+++ datasets.py
@@ -0,0 +1,72 @@
+from __future__ import print_function
+import torch.utils.data as data
+from PIL import Image
+import os
+import os.path
+import errno
+import torch
+import json
+import codecs
+import numpy as np
+import sys
+import torchvision.transforms as transforms
+import argparse
+import json
+import random
+
+
+class PartDataset(data.Dataset):
+    def __init__(self, root, train = True):
+        self.root = root
+        self.path=os.listdir(root)
+        self.maxn=100
+
+        if train:
+            self.path = self.path[:int(len(self.path) * 0.9)]
+        else:
+            self.path = self.path[int(len(self.path) * 0.9):]
+
+    def __getitem__(self, index):
+        pathi = self.path[index]
+        f = open(os.path.join(self.root, pathi), 'r')
+        sdf = list(map(float, f.readline().split()))
+        sdf = np.array(sdf).reshape(self.maxn, self.maxn, self.maxn)
+        out_sdf = np.zeros((1, 64,64,64))
+        aug1=random.random()
+        aug2=random.random()
+        aug3=random.random()
+        for i in range(64):
+            i1=i
+            if aug1<0.5:
+                i1=63-i
+            for j in range(64):
+                j1=j
+                if aug2<0.5:
+                    j1=63-j
+                for k in range(64):
+                    k1=k
+                    if aug3<0.5:
+                        k1=63-k
+                    out_sdf[0, i, j, k] = sdf[18+i1,18+j1,18+k1]
+        #truncate
+        #out_sdf=np.clip(out_sdf,-20,20)
+        in_voxel = np.sign(out_sdf)
+
+        in_voxel = torch.from_numpy(in_voxel)
+        out_sdf = torch.from_numpy(out_sdf)
+
+        in_voxel = in_voxel.float()
+        out_sdf = out_sdf.float()
+
+        return in_voxel, out_sdf
+
+    def __len__(self):
+        return len(self.path)
+
+
+if __name__ == '__main__':
+    print('test')
+    d = PartDataset(root = 'sdf',train=True)
+    print(len(d))
+    inn,out = d[0]
+    print(inn.size(), inn.type(), out.size(),out.type())
---
+++ train_segmentation.py
@@ -0,0 +1,107 @@
+from __future__ import print_function
+import argparse
+import os
+import random
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.backends.cudnn as cudnn
+import torch.optim as optim
+import torch.utils.data
+import torchvision.datasets as dset
+import torchvision.transforms as transforms
+import torchvision.utils as vutils
+from torch.autograd import Variable
+from datasets import PartDataset
+from model import Modified3DUNet
+import torch.nn.functional as F
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--batchSize', type=int, default=32, help='input batch size')
+parser.add_argument('--workers', type=int, help='number of data loading workers', default=1)
+parser.add_argument('--nepoch', type=int, default=25, help='number of epochs to train for')
+parser.add_argument('--nowepoch', type=int, default=0, help='current epoch')
+parser.add_argument('--outf', type=str, default='seg', help='output folder')
+parser.add_argument('--model', type=str, default='', help='model path')
+parser.add_argument('--lr', type=float, default=0.01, help='learning rate')
+
+opt = parser.parse_args()
+print(opt)
+
+opt.manualSeed = random.randint(1, 10000)  # fix seed
+print("Random Seed: ", opt.manualSeed)
+random.seed(opt.manualSeed)
+torch.manual_seed(opt.manualSeed)
+
+dataset = PartDataset(root='..//sdf_newnew', train=True)
+dataloader = torch.utils.data.DataLoader(dataset, batch_size=opt.batchSize,
+                                         shuffle=True, num_workers=int(opt.workers))
+
+test_dataset = PartDataset(root='..//sdf_newnew', train=False)
+testdataloader = torch.utils.data.DataLoader(test_dataset, batch_size=opt.batchSize,
+                                             shuffle=True, num_workers=int(opt.workers))
+
+print(len(dataset), len(test_dataset))
+try:
+    os.makedirs(opt.outf)
+except OSError:
+    pass
+
+
+classifier = Modified3DUNet(in_channels=1, n_classes=1)
+
+if opt.model != '':
+    classifier.load_state_dict(torch.load(opt.model))
+
+optimizer = optim.SGD(classifier.parameters(), lr=opt.lr, momentum=0.9)
+classifier.cuda()
+
+loss_fn = torch.nn.MSELoss(reduce=True, size_average=True)
+
+num_batch = len(dataset) / opt.batchSize
+
+for epoch in range(opt.nowepoch,opt.nowepoch+opt.nepoch):
+    for i, data in enumerate(dataloader, 0):
+        points, target = data
+        points, target = Variable(points), Variable(target)
+        points, target = points.cuda(), target.cuda()
+        optimizer.zero_grad()
+        classifier = classifier.train()
+        pred = classifier(points)
+        pred = pred.view(-1)
+        target = target.view(-1)
+        loss = loss_fn(pred, target)
+        loss.backward()
+        optimizer.step()
+        print('[%d: %d/%d] train loss: %f' % (epoch, i, num_batch, loss.item()))
+        if i % 10 ==0:
+            j,data=next(enumerate(testdataloader,0))
+            points, target = data
+            points, target = Variable(points), Variable(target)
+            points, target = points.cuda(), target.cuda()
+            optimizer.zero_grad()
+            classifier = classifier.train()
+            pred = classifier(points)
+            pred = pred.view(-1)
+            target = target.view(-1)
+            loss = loss_fn(pred, target)
+            print('test loss: %f' % (loss))
+
+        '''if i % 10 == 0:
+            j, data = next(enumerate(testdataloader, 0))
+            points, target = data
+            points, target = Variable(points), Variable(target)
+            points = points.transpose(2,1) 
+            points, target = points.cuda(), target.cuda()
+            classifier = classifier.eval()
+            pred, _ = classifier(points)
+            pred = pred.view(-1, num_classes)
+            target = target.view(-1,1)[:,0] - 1
+
+            loss = F.nll_loss(pred, target)
+            pred_choice = pred.data.max(1)[1]
+            correct = pred_choice.eq(target.data).cpu().sum()
+            print('[%d: %d/%d] %s loss: %f accuracy: %f' %(epoch, i, num_batch, blue('test'), loss.item(), correct.item()/float(opt.batchSize * 2500)))
+    '''
+    torch.save(classifier.state_dict(), '%s/seg_model_%d.pth' % (opt.outf, epoch))
